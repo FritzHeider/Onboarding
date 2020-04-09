483ec5a7b0230a95e0c3ccbec1e16389215e7550
FritzHeider <fritz.heider@students.makeschool.com>
2020-04-08 23:12:37 -0700

    giphy

--- Onboarding/OnboardingPage.swift
+++ Onboarding/OnboardingPage.swift
@@ -4,6 +4,61 @@
 //
 //  Created by Fritz Heider on 4/8/20.
 //  Copyright © 2020 Make School. All rights reserved.
-//
 
-import Foundation
+import UIKit
+
+class OnboardingPage: UIView {
+    
+    var newImage: UIImageView!
+    var button: UIButton!
+    var newLabel: UILabel!
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    func setup() {
+        //        self.backgroundColor = UIColor.purple
+        button = UIButton(frame: .zero)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = UIColor(displayP3Red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        button.setTitle("Continue", for: .normal)
+        self.addSubview(button)
+        
+        
+        newLabel = UILabel(frame: .zero)
+        newLabel.translatesAutoresizingMaskIntoConstraints = false
+        newLabel.text = "this is my label"
+        self.addSubview(newLabel)
+        
+        newImage = UIImageView(frame: .zero)
+        newImage.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(newImage)
+        newImage.image = UIImage(named: "img1")
+        
+        NSLayoutConstraint.activate([
+            newImage.widthAnchor.constraint(equalToConstant: 180),
+            newImage.heightAnchor.constraint(equalTo: newImage.widthAnchor, multiplier: 1/1),
+            newImage.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            newImage.centerYAnchor.constraint(equalTo:  self.centerYAnchor),
+//            newLabel.widthAnchor.constraint(equalToConstant: 180),
+            newLabel.heightAnchor.constraint(equalToConstant: 180),
+            newLabel.centerYAnchor.constraint(equalTo: newImage.centerYAnchor, constant: 100),
+            newLabel.centerXAnchor.constraint(equalTo: newImage.centerXAnchor, constant: 0),
+            button.widthAnchor.constraint(equalToConstant: 100),
+            button.heightAnchor.constraint(equalToConstant: 60),
+            button.centerYAnchor.constraint(equalTo: newImage.centerYAnchor, constant: 200),
+            button.centerXAnchor.constraint(equalTo: newImage.centerXAnchor, constant: 0),
+            
+            
+        ])
+        
+        
+        
+    }
+}
--- Onboarding/ViewController.swift
+++ Onboarding/ViewController.swift
@@ -9,12 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    
+    var scrollView: UIScrollView!
+    var container: UIStackView!
+    let firstView =  OnboardingPage()
+    let secondView = OnboardingPage()
+    let thirdView = OnboardingPage()
+    
+    
+    override func loadView() {
+        super.loadView()
+        
+        scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.isPagingEnabled = true
+        scrollView.showsHorizontalScrollIndicator = false
+        view.addSubview(scrollView)
+        
+        container = UIStackView()
+        container.axis = .horizontal
+        container.spacing = 0
+        container.translatesAutoresizingMaskIntoConstraints = false
+        container.distribution = .fillEqually
+        
+        scrollView.addSubview(container)
+        container.addArrangedSubview(firstView)
+        container.addArrangedSubview(secondView)
+        container.addArrangedSubview(thirdView)
+        
+        scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        scrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        container.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
+        container.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
+        container.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        container.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+        container.heightAnchor.constraint(equalTo: scrollView.heightAnchor).isActive = true
+        
+        firstView.widthAnchor.constraint(equalToConstant: view.frame.size.width).isActive = true
+        secondView.widthAnchor.constraint(equalToConstant: view.frame.size.width).isActive = true
+        thirdView.widthAnchor.constraint(equalToConstant: view.frame.size.width).isActive = true
+        
+        firstView.button.isHidden = true
+        secondView.button.isHidden = true
+          thirdView.button.isEnabled = true
+        
+        
+        firstView.newLabel.text = "Presenting Mobile 1.2"
+        secondView.newLabel.text = "An app to fight the pandemic"
+        thirdView.newLabel.text = "Are you ready for this?"
+        
+        secondView.newImage.image = UIImage(named: "img2")
+        thirdView.newImage.image = UIImage(named: "img3")
+//        
+//        thirdView.button.addTarget(self, action: #selector(goToLogin), for: .touchUpInside)
+//
+//
+        
+        
     }
-
-
 }
-
